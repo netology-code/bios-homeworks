fb866e481ca4273d4444ba86cf55eb472c5ca427
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2019-11-08 12:09:14 +0300

    Implemented template for example

--- 4.2/examples/ios-BiOS_4.2_lecture_example/LectureFramework/Logic.swift
+++ 4.2/examples/ios-BiOS_4.2_lecture_example/LectureFramework/Logic.swift
@@ -3,3 +3,43 @@
 //
 
 import Foundation
+
+class Worker {
+    
+    func toDo(work: () -> Void, completion: (Bool) -> Void) {
+        work()
+        completion(true)
+    }
+}
+
+
+class Wallpapers {
+    
+    let name: String
+    let patternAlignment: Bool
+    
+    init(name: String, patternAlignment: Bool) {
+        self.name = name
+        self.patternAlignment = patternAlignment
+    }
+}
+
+
+class WallpaperTask {
+    
+    let wallpapers: Wallpapers
+    let count: Int
+    
+    init(wallpapers: Wallpapers, count: Int) {
+        self.wallpapers = wallpapers
+        self.count = count
+    }
+}
+
+
+class Office {
+    
+    func order(tasks: [WallpaperTask], at date: Date, completion: (Double) -> Void) {
+        //  some code
+    }
+}
